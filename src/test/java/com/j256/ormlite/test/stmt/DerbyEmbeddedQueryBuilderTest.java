3681f5c74da723fd25fc906ca7afd2e7995babc2
graywatson <graywatson@05e1a9df-29cb-4752-bae5-e712e3cd3cb9>
2011-02-27 21:45:07 +0000

    Added BYTE_ARRAY type and revamped a good bit of the DataType class after adding new tests.  byte[] dataType must be specified.

--- src/test/java/com/j256/ormlite/test/stmt/DerbyEmbeddedQueryBuilderTest.java
+++ src/test/java/com/j256/ormlite/test/stmt/DerbyEmbeddedQueryBuilderTest.java
@@ -1,6 +1,14 @@
 package com.j256.ormlite.test.stmt;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.stmt.JdbcQueryBuilderTest;
+import com.j256.ormlite.stmt.QueryBuilder;
 
 public class DerbyEmbeddedQueryBuilderTest extends JdbcQueryBuilderTest {
 
@@ -8,5 +16,24 @@ public class DerbyEmbeddedQueryBuilderTest extends JdbcQueryBuilderTest {
 	protected void setDatabaseParams() {
 		System.setProperty("derby.stream.error.file", "target/derby.log");
 		databaseUrl = "jdbc:derby:target/ormlitederby;create=true";
+	}
+	
+	@Override
+	@Test
+	public void testOffsetNoLimit() throws Exception {
+		Dao<Foo, Object> dao = createDao(Foo.class, true);
+		Foo foo1 = new Foo();
+		foo1.id = "stuff1";
+		assertEquals(1, dao.create(foo1));
+		Foo foo2 = new Foo();
+		foo2.id = "stuff2";
+		assertEquals(1, dao.create(foo2));
+		assertEquals(2, dao.queryForAll().size());
+
+		QueryBuilder<Foo, Object> qb = dao.queryBuilder();
+		int offset = 1;
+		qb.offset(offset);
+		List<Foo> results = dao.query(qb.prepare());
+		assertEquals(1, results.size());
 	}
 }
