61a885c8f7f3180336811032fae7431462ea4155
graywatson <graywatson@05e1a9df-29cb-4752-bae5-e712e3cd3cb9>
2011-06-03 18:42:42 +0000

    Renamed some testd.

--- src/test/java/com/j256/ormlite/db/HsqldbConnectTest.java
+++ src/test/java/com/j256/ormlite/db/HsqldbConnectTest.java
@@ -4,7 +4,7 @@ package com.j256.ormlite.db;
  * Does tests on a real database connection. I didn't want to have to require someone to have have downloaded derby
  * database to have the ormlite tests work.
  */
-public class HsqldbConnectTypeTest extends HsqldbDatabaseTypeTest {
+public class HsqldbConnectTest extends HsqldbDatabaseTypeTest {
 
 	@Override
 	protected void setDatabaseParams() {
--- src/test/java/com/j256/ormlite/db/SqliteConnectTest.java
+++ src/test/java/com/j256/ormlite/db/SqliteConnectTest.java
@@ -4,7 +4,7 @@ package com.j256.ormlite.db;
  * Does tests on a real database connection. I didn't want to have to require someone to have have downloaded derby
  * database to have the ormlite tests work.
  */
-public class SqliteConnectTypeTest extends SqliteDatabaseTypeTest {
+public class SqliteConnectTest extends SqliteDatabaseTypeTest {
 
 	@Override
 	protected void setDatabaseParams() {
