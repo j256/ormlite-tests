3a53b3d41f482cd6986e949cdb8a4a00c0a7410c
graywatson <graywatson@05e1a9df-29cb-4752-bae5-e712e3cd3cb9>
2011-09-22 13:50:02 +0000

    Cp.

--- src/test/java/com/j256/ormlite/dao/SqliteBaseDaoImplTest.java
+++ src/test/java/com/j256/ormlite/dao/SqliteBaseDaoImplTest.java
@@ -1,9 +1,27 @@
 package com.j256.ormlite.dao;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
 public class SqliteBaseDaoImplTest extends JdbcBaseDaoImplTest {
 
 	@Override
 	protected void setDatabaseParams() {
 		databaseUrl = "jdbc:sqlite:";
+	}
+
+	@Test
+	public void testStoreUnicode() throws Exception {
+		Dao<Foo, Object> dao = createDao(Foo.class, true);
+		Foo foo = new Foo();
+		String unicodeString = "上海";
+		foo.stuff = unicodeString;
+		assertEquals(1, dao.create(foo));
+
+		Foo result = dao.queryForId(foo.id);
+		assertNotNull(result);
+		assertEquals(unicodeString, result.stuff);
 	}
 }
