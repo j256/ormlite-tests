d7218d87a96c6b5e9e908ec6564a1ad8fa085464
graywatson <graywatson@05e1a9df-29cb-4752-bae5-e712e3cd3cb9>
2011-10-06 20:41:41 +0000

    Added update tests for sqlite.

--- src/test/java/com/j256/ormlite/dao/SqliteBaseDaoImplTest.java
+++ src/test/java/com/j256/ormlite/dao/SqliteBaseDaoImplTest.java
@@ -3,7 +3,12 @@ package com.j256.ormlite.dao;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import java.util.List;
+
 import org.junit.Test;
+
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
 
 public class SqliteBaseDaoImplTest extends JdbcBaseDaoImplTest {
 
@@ -23,5 +28,36 @@ public class SqliteBaseDaoImplTest extends JdbcBaseDaoImplTest {
 		Foo result = dao.queryForId(foo.id);
 		assertNotNull(result);
 		assertEquals(unicodeString, result.stuff);
+	}
+
+	@Test
+	public void testAlterTable() throws Exception {
+		Dao<Foo, Object> dao = createDao(Foo.class, true);
+		Dao<FooAge, Object> ageDao = createDao(FooAge.class, false);
+		Foo foo = new Foo();
+		String stuff = "feowpjfpewf";
+		foo.stuff = stuff;
+		assertEquals(1, dao.create(foo));
+		String newCol = "age";
+		int newColDefault = 1;
+		dao.executeRaw("ALTER TABLE `footable` ADD COLUMN `" + newCol + "` INTEGER DEFAULT " + newColDefault);
+
+		List<FooAge> list = ageDao.queryForEq(newCol, newColDefault);
+		assertEquals(1, list.size());
+		assertEquals(newColDefault, list.get(0).age);
+
+		int newVal = 123231;
+		dao.executeRaw("UPDATE `footable` SET `" + newCol + "` = " + newVal);
+		list = ageDao.queryForEq(newCol, newVal);
+		assertEquals(1, list.size());
+		assertEquals(newVal, list.get(0).age);
+	}
+
+	@DatabaseTable(tableName = FOO_TABLE_NAME)
+	protected static class FooAge extends Foo {
+		@DatabaseField
+		public int age;
+		public FooAge() {
+		}
 	}
 }
