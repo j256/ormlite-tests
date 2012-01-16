964e21cdef34503d64ea88382ce52ec0619548e1
graywatson <graywatson@05e1a9df-29cb-4752-bae5-e712e3cd3cb9>
2012-01-16 16:36:22 +0000

    Fixed restriction on auto-incremented SQLite ids not Long-s.  Thanks to John Kristian. Fixes bug #3474479.

--- src/test/java/com/j256/ormlite/db/SqliteConnectTest.java
+++ src/test/java/com/j256/ormlite/db/SqliteConnectTest.java
@@ -1,5 +1,13 @@
 package com.j256.ormlite.db;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotSame;
+
+import org.junit.Test;
+
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.field.DatabaseField;
+
 /**
  * Does tests on a real database connection. I didn't want to have to require someone to have have downloaded derby
  * database to have the ormlite tests work.
@@ -14,5 +22,49 @@ public class SqliteConnectTest extends SqliteDatabaseTypeTest {
 	@Override
 	protected boolean isDriverClassExpected() {
 		return true;
+	}
+
+	@Test
+	public void testGeneratedIdInt() throws Exception {
+		Dao<IntAutoIncrement, Object> dao = createDao(IntAutoIncrement.class, true);
+		IntAutoIncrement foo = new IntAutoIncrement();
+		foo.stuff = "fwpojfpwejf";
+		assertEquals(0, foo.id);
+		assertEquals(1, dao.create(foo));
+		assertEquals(1, foo.id);
+		IntAutoIncrement result = dao.queryForId(foo.id);
+		assertNotSame(foo, result);
+		assertEquals(foo.stuff, result.stuff);
+	}
+
+	@Test
+	public void testGeneratedIdLong() throws Exception {
+		Dao<LongAutoIncrement, Object> dao = createDao(LongAutoIncrement.class, true);
+		LongAutoIncrement foo = new LongAutoIncrement();
+		foo.stuff = "fwpojfpwejf";
+		assertEquals(0L, foo.id);
+		assertEquals(1, dao.create(foo));
+		assertEquals(1L, foo.id);
+		LongAutoIncrement result = dao.queryForId(foo.id);
+		assertNotSame(foo, result);
+		assertEquals(foo.stuff, result.stuff);
+	}
+
+	protected static class IntAutoIncrement {
+		@DatabaseField(generatedId = true)
+		int id;
+		@DatabaseField
+		String stuff;
+		public IntAutoIncrement() {
+		}
+	}
+
+	protected static class LongAutoIncrement {
+		@DatabaseField(generatedId = true)
+		long id;
+		@DatabaseField
+		String stuff;
+		public LongAutoIncrement() {
+		}
 	}
 }
