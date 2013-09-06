39291adc7087c85f2e79f58ac3241156a2dbaeec
Gray Watson <gray.github@mailnull.com>
2013-09-06 08:30:43 -0400

    Formatting.

--- src/test/java/com/j256/ormlite/misc/SqliteTransactionManagerTest.java
+++ src/test/java/com/j256/ormlite/misc/SqliteTransactionManagerTest.java
@@ -9,7 +9,7 @@ public class SqliteTransactionManagerTest extends BaseTransactionManagerTest {
 	protected void setDatabaseParams() {
 		databaseUrl = "jdbc:sqlite:target/sqliteTransactionManagerTest";
 	}
-	
+
 	@Override
 	@Test
 	@Ignore("not sure why this doesn't work but I can't get it to go either way")
