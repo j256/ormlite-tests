78b75e003eae20b85526f663f408679d86ee2bcf
Gray Watson <gray.github@mailnull.com>
2014-01-02 18:00:03 -0500

    Added specific mysql tests to watch for schema mismatch issues around generated ids.

--- src/test/java/com/j256/ormlite/db/MysqlConnectTest.java
+++ src/test/java/com/j256/ormlite/db/MysqlConnectTest.java
@@ -1,10 +1,20 @@
 package com.j256.ormlite.db;
 
+import java.sql.SQLException;
+
+import org.junit.Test;
+
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
 /**
  * Does tests on a real database connection. I didn't want to have to require someone to have have downloaded derby
  * database to have the ormlite tests work.
  */
 public class MysqlConnectTest extends MysqlDatabaseTypeTest {
+
+	private static final String FOOINT_TABLE_NAME = "fooint";
 
 	@Override
 	protected void setDatabaseParams() {
@@ -18,5 +28,34 @@ public class MysqlConnectTest extends MysqlDatabaseTypeTest {
 	@Override
 	protected boolean isDriverClassExpected() {
 		return true;
+	}
+
+	@Test(expected = SQLException.class)
+	public void testGeneratedIdNoReturn() throws Exception {
+		createDao(FooNotGeneratedId.class, true);
+		Dao<FooInt, Object> genDao = createDao(FooInt.class, false);
+		FooInt foo = new FooInt();
+		foo.stuff = "hello";
+		genDao.create(foo);
+	}
+
+	@DatabaseTable(tableName = FOOINT_TABLE_NAME)
+	protected static class FooInt {
+		@DatabaseField(generatedId = true)
+		public int id;
+		@DatabaseField
+		public String stuff;
+		FooInt() {
+		}
+	}
+
+	@DatabaseTable(tableName = FOOINT_TABLE_NAME)
+	protected static class FooNotGeneratedId {
+		@DatabaseField
+		public int id;
+		@DatabaseField
+		public String stuff;
+		FooNotGeneratedId() {
+		}
 	}
 }
